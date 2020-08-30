5d77dfa0c56dec3fc99f48f5498330efd90c89b5
Jason Easterly <jason.easterly11@gmail.com>
2020-08-30 17:02:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,10 +18,15 @@ end
 
 mixed_calls = ["Cloud!", "earth", "Wind!", "cat", "Heart!"]
 def find_valid_calls(planeteer_calls)
+ 
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+<<<<<<< HEAD
 
 planeteer_calls.find {|v| valid_calls.include?(v)}
 
+=======
+planeteer_calls.find {|v| p v.include?(valid_calls)}
+>>>>>>> c8c4660384b97b4e4e44e779a7c4aed031fca333
 end 
 
 
